b0d532f04c507e6065895c256a06138fe21b0ea6
keepitwiel <keepitwiel@protonmail.com>
2023-10-06 10:40:46 +0200

    Clean up solver + demo

--- src/demo.py
+++ src/demo.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib import colors
 from height_map import generate_height_map
 
-from solver import update
+from solver import update_water
 
 
 SEED = 116
@@ -16,41 +16,32 @@ SLOPE_AMPLITUDE = 64
 SMOOTHNESS = 0.6
 RAINFALL = 0.1
 EVAP = 0.0001
-EROSION = 0.01
 
 
 def main():
     z = generate_height_map(MAP_HEIGHT, MAP_WIDTH, SEED, SMOOTHNESS) * RANDOM_AMPLITUDE
     z += np.linspace(0, 1, MAP_HEIGHT).reshape(-1, 1).dot(np.ones(MAP_WIDTH).reshape(1, -1)) * SLOPE_AMPLITUDE
     z -= SEA_LEVEL
-    dz = np.zeros_like(z)
-    z0 = z.copy()
-
     r = np.zeros_like(z)
-    # r[1:MAP_HEIGHT-1, 1:MAP_WIDTH-1] = RAINFALL / (MAP_HEIGHT * MAP_WIDTH)
     r[MAP_HEIGHT - 8, MAP_WIDTH // 2 - 8] = RAINFALL
     h = -np.minimum(0.0, z)
-    dh = np.zeros_like(h)
-    flux = np.zeros_like(h)
-    flux_out = np.zeros_like(h)
+    dh = np.zeros_like(z)
+    H = np.zeros_like(z)
 
-    fig, axes = plt.subplots(1, 4, figsize=(12, 4))
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4))
     axes[0].set_title("Terrain altitude")
     cmap = colors.ListedColormap(["blue", "cyan", "green", "yellow", "orange", "red"])
     bounds = [-128, 0, 16, 32, 48, 64, 128]
     norm = colors.BoundaryNorm(bounds, cmap.N)
-    axes[1].set_title(f"Total water: {np.sum(h):2.2f}")
-    axes[2].set_title("Flux")
-    axes[3].set_title("Terrain displacement")
+    axes[2].set_title("Terrain + water surface altitude")
 
     for count in tqdm(range(100000)):
-        update(z, dz, h, dh, flux, flux_out, r, evap=EVAP, erosion=EROSION, dt=0.1)
+        update_water(H, z, h, dh, r, dt=0.1, evap=EVAP)
         if count % 1000 == 0:
             axes[0].imshow(z, cmap=cmap, norm=norm)
-            axes[1].imshow(h, vmax=RAINFALL * 100)
-            axes[1].set_title(f"Total water: {np.sum(h):2.2f}")
-            axes[2].imshow(flux_out, vmax=RAINFALL * 10)
-            axes[3].imshow(z - z0, vmin=-10, vmax=10)
+            axes[1].imshow(h, vmax=RAINFALL)
+            axes[1].set_title(f"Iteration: {count}, Total water: {np.sum(h):2.2f}")
+            axes[2].imshow(H, vmin=-1, vmax=SLOPE_AMPLITUDE + RANDOM_AMPLITUDE)
             plt.pause(0.001)
         count += 1
 
--- src/solver.py
+++ src/solver.py
@@ -2,65 +2,32 @@ import numpy as np
 from numba import njit
 
 
-def update(z, dz, h, dh, flux, flux_out, r, evap, erosion, dt):
-    update_water(z, h, dh, flux, r, dt)
-    diffuse_terrain(z, dz, h, flux, erosion, dt)
-    evaporate(h, flux, evap, dt)
-    flux_out[:, :] = flux[:, :]
-    flux.fill(0.0)
-
-
 @njit
-def update_water(z, h, dh, flux, r, dt):
-    dh += r
+def update_water(H, z, h, dh, r, dt, evap):
+    dh += r * dt
     height, width = z.shape
+    H[:, :] = z[:, :] + h[:, :]
     for j in range(1, height-1):
         for i in range(1, width-1):
             h_ = h[j, i]
-            z_ = z[j, i]
-
             if h_ > 0:
-                left = z[j, i-1] + h[j, i-1] < z_ + h_ if i - 1 > 0 else False
-                right = z[j, i+1] + h[j, i+1] < z_ + h_ if i + 1 < width else False
-                up = z[j-1, i] + h[j-1, i] < z_ + h_ if j - 1 > 0 else False
-                down = z[j+1, i] + h[j+1, i] < z_ + h_ if j + 1 < width else False
-
+                H_ = H[j, i]
+                left  = max(H_ - H[j, i-1], 0) if i - 1 > 0 else 0
+                right = max(H_ - H[j, i+1], 0) if i + 1 < width else 0
+                up    = max(H_ - H[j-1, i], 0) if j - 1 > 0 else 0
+                down  = max(H_ - H[j+1, i], 0) if j + 1 < width else 0
                 total = left + right + up + down
-
                 if total > 0:
-                    delta = h_ / total * dt
-                    dh[j, i] -= h_ * dt
+                    # tau is a factor that prevents large oscillations when the target h's have high depth
+                    # if target h's are all 0, then tau = 1
+                    # if target h's are all large, then tau tends to 0
+                    tau = np.exp(-(left * h[j, i-1] + right * h[j, i+1] + up * h[j-1, i] + down * h[j+1, i]))
+                    delta = h_ / total * dt * tau
+                    dh[j, i] -= total * delta
                     dh[j, i-1] += left * delta
                     dh[j, i+1] += right * delta
                     dh[j-1, i] += up * delta
                     dh[j+1, i] += down * delta
-
-                    flux[j, i] += h_ * dt
-                    flux[j, i-1] += left * delta
-                    flux[j, i+1] += right * delta
-                    flux[j-1, i] += up * delta
-                    flux[j+1, i] += down * delta
-
     h += dh
     dh.fill(0.0)
-
-
-@njit
-def diffuse_terrain(z, dz, h, flux, erosion, dt):
-    height, width = z.shape
-    for j in range(1, height-1):
-        for i in range(1, width-1):
-            delta = (flux[j, i]) / (h[j, i]**2 + 1) * erosion * dt
-            dz[j, i] -= delta * 4
-            dz[j, i - 1] += delta
-            dz[j, i + 1] += delta
-            dz[j - 1, i] += delta
-            dz[j + 1, i] += delta
-    z += dz
-    dz.fill(0.0)
-
-
-@njit
-def evaporate(h, flux, evap, dt):
-    delta = np.minimum(evap * dt, h / (flux + 1))
-    h -= np.minimum(h, delta)
+    h -= np.minimum(h, evap * dt)

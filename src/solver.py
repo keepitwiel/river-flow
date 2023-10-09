5fa144c03ac21701adfb61568e1218bfcbbe43f9
keepitwiel <keepitwiel@protonmail.com>
2023-10-09 16:56:19 +0200

    Make solver faster

--- src/solver.py
+++ src/solver.py
@@ -3,19 +3,23 @@ from numba import njit
 
 
 @njit
-def update_water(H, z, h, dh, r, dt, evap):
-    dh += r * dt
+def update_water(z, h, dh, r, dt, evap):
     height, width = z.shape
-    H[:, :] = z[:, :] + h[:, :]
     for j in range(1, height-1):
         for i in range(1, width-1):
             h_ = h[j, i]
             if h_ > 0:
-                H_ = H[j, i]
-                left  = max(H_ - H[j, i-1], 0) if i - 1 > 0 else 0
-                right = max(H_ - H[j, i+1], 0) if i + 1 < width else 0
-                up    = max(H_ - H[j-1, i], 0) if j - 1 > 0 else 0
-                down  = max(H_ - H[j+1, i], 0) if j + 1 < width else 0
+                h[j, i] += r[j, i] * dt
+                H_ = h_ + z[j, i]
+                H_left = h[j, i-1] + z[j, i-1]
+                H_right = h[j, i+1] + z[j, i+1]
+                H_up = h[j-1, i] + z[j-1, i]
+                H_down = h[j-1, i] + z[j+1, i]
+
+                left  = max(H_ - H_left, 0) if i - 1 > 0 else 0
+                right = max(H_ - H_right, 0) if i + 1 < width else 0
+                up    = max(H_ - H_up, 0) if j - 1 > 0 else 0
+                down  = max(H_ - H_down, 0) if j + 1 < width else 0
                 total = left + right + up + down
                 if total > 0:
                     delta = h_ / total * dt
@@ -26,4 +30,5 @@ def update_water(H, z, h, dh, r, dt, evap):
                     dh[j+1, i] += down * delta
     h += dh
     dh.fill(0.0)
-    h -= np.minimum(h, evap * dt)
+    if evap > 0:
+        h -= np.minimum(h, evap * dt)
